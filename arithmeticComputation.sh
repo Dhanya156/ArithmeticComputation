19898159ffe7a643e23e67252e4128029a30068e
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-09-10 07:03:41 -0700

    Added:Computation Of Multiplication And Addition

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -4,3 +4,6 @@ echo "Welcome to Sorting Arithmetic Computation Problem "
 read -p "Enter first input " a
 read -p "Enter second input " b
 read -p "Enter third input " c
+
+Compute1=`awk 'BEGIN{printf("%.2f",'$a' + '$b' * '$c')}'`
+echo  $Compute1
